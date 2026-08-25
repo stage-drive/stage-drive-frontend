caa3f134bb4a9fb6dbb91cebe3833e606561800a
Kateryna Rostovtseva <141571972+KaterynaRostovtseva@users.noreply.github.com>
2026-08-25 10:17:30 +0300

    fix: Setting up the RTK Query API

--- src/store/api/baseApi.ts
+++ src/store/api/baseApi.ts
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+
+const baseUrl = import.meta.env.VITE_API_URL;
+
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.example.com/' }), 
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: () => ({}),
 });
